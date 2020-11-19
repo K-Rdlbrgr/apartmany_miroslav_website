c946fac171a1fdd1e945cfc482f26056742b948c
Kevin Riedlberger <34638@novasbe.pt>
2020-11-19 11:28:52 +0100

    Added and drafted all final tabs and images

--- apartmany_project/apartmany/views.py
+++ apartmany_project/apartmany/views.py
@@ -6,51 +6,71 @@ from .forms import CommentForm, ContactForm, ReservationForm
 import numpy as np
 
 # Create your views here.
+
+
 def home(request):
     return render(request, 'apartmany/index.html')
 
+
 def studios(request):
-    return render(request, 'apartmany/studios.html')
+    ranges = {'range_s1': range(2, 6),
+              'range_s2': range(2, 4),
+              'range_s3': range(2, 6),
+              'range_s4': range(2, 7),
+              'range_s5': range(2, 8),
+              'range_s6': range(2, 8),
+              'range_s8': range(2, 4),
+              'range_s9': range(2, 9),
+              'range_s10': range(2, 5)}
+    return render(request, 'apartmany/studios.html', ranges)
+
 
 def apartments(request):
-    return render(request, 'apartmany/apartments.html')
+    ranges = {'range_a7': range(2, 8),
+              'range_a11': range(2, 14),
+              'range_a12': range(2, 10),
+              'range_a13': range(2, 13),
+              'range_a14': range(2, 17)}
+    return render(request, 'apartmany/apartments.html', ranges)
+
 
 def surroundings(request):
     return render(request, 'apartmany/surroundings.html')
 
+
 def guestbook(request):
-    
+
     comments = Comment.objects.all()
     temp_ratings = []
-    
+
     for comment in comments:
         temp_ratings.append(comment.rating)
-        
+
     average_rating = round(np.average(temp_ratings), 1)
-    
+
     ratings = {
         "average": average_rating,
         "amount": len(temp_ratings),
         "summary": {"five": {"tag": "Excellent",
                              "amount": temp_ratings.count(5),
-                             "percentage": str((temp_ratings.count(5)/len(temp_ratings))*100)+'%'},
+                             "percentage": str((temp_ratings.count(5) / len(temp_ratings)) * 100) + '%'},
                     "four": {"tag": "Good",
                              "amount": temp_ratings.count(4),
-                             "percentage": str((temp_ratings.count(4)/len(temp_ratings))*100)+'%'},
+                             "percentage": str((temp_ratings.count(4) / len(temp_ratings)) * 100) + '%'},
                     "three": {"tag": "Average",
                               "amount": temp_ratings.count(3),
-                              "percentage": str((temp_ratings.count(3)/len(temp_ratings))*100)+'%'},
+                              "percentage": str((temp_ratings.count(3) / len(temp_ratings)) * 100) + '%'},
                     "two": {"tag": "Poor",
                             "amount": temp_ratings.count(2),
-                            "percentage": str((temp_ratings.count(2)/len(temp_ratings))*100)+'%'},
+                            "percentage": str((temp_ratings.count(2) / len(temp_ratings)) * 100) + '%'},
                     "one": {"tag": "Terrible",
                             "amount": temp_ratings.count(1),
-                            "percentage": str((temp_ratings.count(1)/len(temp_ratings))*100)+'%'}
+                            "percentage": str((temp_ratings.count(1) / len(temp_ratings)) * 100) + '%'}
                     }
     }
-    
+
     print(ratings)
-    
+
     new_comment = None
     # Comment posted
     if request.method == 'POST':
@@ -71,17 +91,20 @@ def guestbook(request):
         'form': comment_form,
         'ratings': ratings
     }
-    
+
     return render(request, 'apartmany/guestbook.html', context)
+
 
 def gallery(request):
     return render(request, 'apartmany/gallery.html')
 
+
 def prices_and_faqs(request):
     return render(request, 'apartmany/prices_and_faqs.html')
 
+
 def contact(request):
-    
+
     if request.method == 'POST':
         if 'Submit-Contact' in request.POST:
             # form = ContactForm(data=request.POST)
@@ -90,16 +113,16 @@ def contact(request):
             email = request.POST['Email']
             subject = request.POST['Subject']
             message = request.POST['Message']
-            
+
             send_mail(f'Contact Request: {subject}',
-                    f'From: {name}\nEmail: {email}\n\n\n{message}',
-                    settings.EMAIL_HOST_USER,
-                    ['kevin.riedlberger@novasbe.pt'],
-                    fail_silently=False)
-            
+                      f'From: {name}\nEmail: {email}\n\n\n{message}',
+                      settings.EMAIL_HOST_USER,
+                      ['kevin.riedlberger@novasbe.pt'],
+                      fail_silently=False)
+
             message = f'Thank you very much for your contact request {name}! We will contact you as oon as possible.'
-            return render(request, 'apartmany/contact.html', {'message':message})
-            
+            return render(request, 'apartmany/contact.html', {'message': message})
+
         elif 'Submit-Reservation' in request.POST:
             # form = ReservationForm(data=request.POST)
             # if form.is_valid():
@@ -111,16 +134,16 @@ def contact(request):
             end = request.POST['End_Date']
             guests = request.POST['Guests']
             message = request.POST['Message']
-            
+
             send_mail(f'Reservation Request: {name}',
-                    f'From: {name}\nEmail: {email}\n\n\nAccomodation Type: {type}\nAccomodation Number: {number}\nDate: {start} - {end}\nNumber Of Guests: {guests}\n\n\n{message}',
-                    settings.EMAIL_HOST_USER,
-                    ['kevin.riedlberger@novasbe.pt'],
-                    fail_silently=False)
-            
+                      f'From: {name}\nEmail: {email}\n\n\nAccomodation Type: {type}\nAccomodation Number: {number}\nDate: {start} - {end}\nNumber Of Guests: {guests}\n\n\n{message}',
+                      settings.EMAIL_HOST_USER,
+                      ['kevin.riedlberger@novasbe.pt'],
+                      fail_silently=False)
+
             message = f'Thank you very much for your reservation request {name}! We will contact you as oon as possible.'
-            
-            return render(request, 'apartmany/contact.html', {'message':message})
-    
+
+            return render(request, 'apartmany/contact.html', {'message': message})
+
     print('We went this route, without')
     return render(request, 'apartmany/contact.html')
