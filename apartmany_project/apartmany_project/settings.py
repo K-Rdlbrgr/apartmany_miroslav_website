585fa58c23a69f0d8eadd4006465fe326eb3e193
K-Rdlbrgr <34638@novasbe.pt>
2022-05-02 23:28:15 +0200

    Update settings

--- apartmany_project/apartmany_project/settings.py
+++ apartmany_project/apartmany_project/settings.py
@@ -136,7 +136,7 @@ LANGUAGES = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 
